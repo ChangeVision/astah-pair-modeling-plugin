20d20540affca5865fa44ed599cd6d5092478b10
HyodaKazuaki <kazuaki@ex-t.jp>
2021-04-02 17:13:55 +0900

    Refactor add transaction process to outside of process method

--- src/main/java/jp/ex_t/kazuaki/change_vision/event_listener/ClassDiagramEventListener.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/event_listener/ClassDiagramEventListener.kt
@@ -81,150 +81,62 @@ class ClassDiagramEventListener(private val mqttPublisher: MqttPublisher): IEven
             logger.debug("Op: $operation -> ")
             when (val entity = it.entity) {
                 is IClassDiagram -> {
-                    val owner = entity.owner as INamedElement
-                    val createClassDiagram = CreateClassDiagram(entity.name, owner.name)
-                    createTransaction.operations.add(createClassDiagram)
-                    logger.debug("${entity.name}(IClassDiagram)")
+                    createTransaction.operations
+                        .add(createClassDiagram(entity))
                     break
                 }
                 is IClass -> {
-                    val parentPackage = entity.owner as IPackage
-                    val createClassModel = CreateClassModel(entity.name, parentPackage.name, entity.stereotypes.toList())
-                    createTransaction.operations.add(createClassModel)
-                    logger.debug("${entity.name}(IClass)")
+                    createTransaction.operations
+                        .add(createClassModel(entity))
                 }
                 is IAssociation -> {
-                    val sourceClass = entity.memberEnds.first().owner
-                    val destinationClass = entity.memberEnds.last().owner
-                    when (sourceClass) {
-                        is IClass -> {
-                            when (destinationClass) {
-                                is IClass -> {
-                                    val sourceClassNavigability = entity.memberEnds.first().navigability
-                                    val destinationClassNavigability = entity.memberEnds.last().navigability
-                                    val createAssociationModel = CreateAssociationModel(
-                                        sourceClass.name,
-                                        sourceClassNavigability,
-                                        destinationClass.name,
-                                        destinationClassNavigability,
-                                        entity.name,
-                                    )
-                                    createTransaction.operations.add(createAssociationModel)
-                                    logger.debug("${sourceClass.name}(IClass, $sourceClassNavigability) - ${entity.name}(IAssociation) - ${destinationClass.name}(IClass, $destinationClassNavigability)")
-                                }
-                                else -> {
-                                    logger.debug("${sourceClass.name}(IClass) - ${entity.name}(IAssociation) - $destinationClass(Unknown)")
-                                }
-                            }
-                        }
-                        else -> {
-                            logger.debug("$sourceClass(Unknown) - ${entity.name}(IAssociation) - $destinationClass(Unknown)")
-                        }
-                    }
+                    createTransaction.operations
+                        .add(
+                            createAssociationModel(entity)
+                                ?: continue
+                        )
                 }
                 is IGeneralization -> {
-                    val superClass = entity.superType
-                    val subClass = entity.subType
-                    val createGeneralizationModel = CreateGeneralizationModel(superClass.name, subClass.name, entity.name)
-                    createTransaction.operations.add(createGeneralizationModel)
-                    logger.debug("${superClass.name}(IClass) -> ${entity.name}(IGeneralization) - ${subClass.name}(IClass)")
+                    createTransaction.operations
+                        .add(createGeneralizationModel(entity))
                 }
                 is IRealization -> {
-                    val supplierClass = entity.supplier
-                    val clientClass = entity.client
-                    val createRealizationModel = CreateRealizationModel(supplierClass.name, clientClass.name, entity.name)
-                    createTransaction.operations.add(createRealizationModel)
-                    logger.debug("${supplierClass.name}(IClass) -> ${entity.name}(IRealization) -> ${clientClass.name}(IClass)")
+                    createTransaction.operations
+                        .add(createRealizationModel(entity))
                 }
                 is IOperation -> {
-                    when (val owner = entity.owner) {
-                        is IClass -> {
-                            val createOperation = CreateOperation(owner.name, entity.name, entity.returnTypeExpression)
-                            createTransaction.operations.add(createOperation)
-                            logger.debug("${entity.name}(IOperation) - $owner(IClass)")
-                        }
-                        else -> {
-                            logger.debug("${entity.name}(IOperation) - $owner(Unknown)")
-                        }
-                    }
+                    createTransaction.operations
+                        .add(
+                            createOperation(entity)
+                                ?: continue
+                        )
                 }
                 is IAttribute -> {
-                    when (val owner = entity.owner) {
-                        is IClass -> {
-                            val createAttribute = CreateAttribute(owner.name, entity.name, entity.typeExpression)
-                            createTransaction.operations.add(createAttribute)
-                            logger.debug("${entity.name}(IAttribute) - ${owner}(IClass)")
-                        }
-                        else -> {
-                            logger.debug("${entity.name}(IAttribute) - ${owner}(Unknown)")
-                        }
-                    }
+                    createTransaction.operations
+                        .add(
+                            createAttribute(entity)
+                                ?: continue
+                        )
                 }
-//                    is IModel -> {
-//                        println("${entity.name}(IModel)")
-//                    }
-//                    is IPresentation -> {
-//                        println("${entity.label}(IPresentation)")
-//                    }
+//                is IModel -> {
+//                    println("${entity.name}(IModel)")
+//                }
+//                is IPresentation -> {
+//                    println("${entity.label}(IPresentation)")
+//                }
                 is INodePresentation -> {
-                    when (val diagram = entity.diagram) {
-                        is IClassDiagram -> {
-                            when (val model = entity.model) {
-                                is IClass -> {
-                                    // TODO: クラスとインタフェースをINodePresentationのプロパティで見分ける
-                                    val location = Pair(entity.location.x, entity.location.y)
-                                    val createClassPresentation = CreateClassPresentation(model.name, location, diagram.name)
-                                    createTransaction.operations.add(createClassPresentation)
-                                    logger.debug("${entity.label}(INodePresentation)::${model.name}(IClass, ${Pair(entity.width, entity.height)} at ${entity.location})")
-                                }
-                                else -> {
-                                    logger.debug("${entity.label}(INodePresentation) - $model(Unknown)")
-                                }
-                            }
-                        }
-                        else -> {
-                            logger.debug("${entity.label}(Unknown)")
-                        }
-                    }
+                    createTransaction.operations
+                        .add(
+                            createClassPresentation(entity)
+                                ?: continue
+                        )
                 }
                 is ILinkPresentation -> {
-                    val source = entity.source.model
-                    val target = entity.target.model
-                    logger.debug("Model: ${entity.model::class.java}")
-                    when (source) {
-                        is IClass -> {
-                            when (target) {
-                                is IClass -> {
-                                    when (entity.model) {
-                                        is IAssociation -> {
-                                            val createLinkPresentation = CreateLinkPresentation(source.name, target.name, "Association", entity.diagram.name)
-                                            createTransaction.operations.add(createLinkPresentation)
-                                            logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::IAssociation) - ${target.name}(IClass)")
-                                        }
-                                        is IGeneralization -> {
-                                            val createLinkPresentation = CreateLinkPresentation(source.name, target.name, "Generalization", entity.diagram.name)
-                                            createTransaction.operations.add(createLinkPresentation)
-                                            logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::IGeneralization) - ${target.name}(IClass)")
-                                        }
-                                        is IRealization -> {
-                                            val createLinkPresentation = CreateLinkPresentation(source.name, target.name, "Realization", entity.diagram.name)
-                                            createTransaction.operations.add(createLinkPresentation)
-                                            logger.debug("${source.name}(IClass, interface) - ${entity.label}(ILinkPresentation::IRealization) - ${target.name}(IClass)")
-                                        }
-                                        else -> {
-                                            logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::Unknown) - ${target.name}(IClass)")
-                                        }
-                                    }
-                                }
-                                else -> {
-                                    logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation) - $target(Unknown)")
-                                }
-                            }
-                        }
-                        else -> {
-                            logger.debug("$source(Unknown) - ${entity.label}(ILinkPresentation) - $target(Unknown)")
-                        }
-                    }
+                    createTransaction.operations
+                        .add(
+                            createLinkPresentation(entity)
+                                ?: continue
+                        )
                 }
                 else -> {
                     logger.debug("$entity(Unknown)")
@@ -393,6 +305,154 @@ class ClassDiagramEventListener(private val mqttPublisher: MqttPublisher): IEven
             }
             else -> {
                 logger.debug("${model}(INodePresentation, Unknown)")
+                null
+            }
+        }
+    }
+
+    private fun createClassDiagram(entity: IClassDiagram): CreateClassDiagram {
+        val owner = entity.owner as INamedElement
+        val createClassDiagram = CreateClassDiagram(entity.name, owner.name)
+        logger.debug("${entity.name}(IClassDiagram)")
+        return createClassDiagram
+    }
+
+    private fun createClassModel(entity: IClass): CreateClassModel {
+        val parentPackage = entity.owner as IPackage
+        val createClassModel = CreateClassModel(entity.name, parentPackage.name, entity.stereotypes.toList())
+        logger.debug("${entity.name}(IClass)")
+        return createClassModel
+    }
+
+    private fun createAssociationModel(entity: IAssociation): CreateAssociationModel? {
+        val sourceClass = entity.memberEnds.first().owner
+        val destinationClass = entity.memberEnds.last().owner
+        return when (sourceClass) {
+            is IClass -> {
+                when (destinationClass) {
+                    is IClass -> {
+                        val sourceClassNavigability = entity.memberEnds.first().navigability
+                        val destinationClassNavigability = entity.memberEnds.last().navigability
+                        logger.debug("${sourceClass.name}(IClass, $sourceClassNavigability) - ${entity.name}(IAssociation) - ${destinationClass.name}(IClass, $destinationClassNavigability)")
+                        CreateAssociationModel(
+                            sourceClass.name,
+                            sourceClassNavigability,
+                            destinationClass.name,
+                            destinationClassNavigability,
+                            entity.name,
+                        )
+                    }
+                    else -> {
+                        logger.debug("${sourceClass.name}(IClass) - ${entity.name}(IAssociation) - $destinationClass(Unknown)")
+                        null
+                    }
+                }
+            }
+            else -> {
+                logger.debug("$sourceClass(Unknown) - ${entity.name}(IAssociation) - $destinationClass(Unknown)")
+                null
+            }
+        }
+    }
+
+    private fun createGeneralizationModel(entity: IGeneralization): CreateGeneralizationModel {
+        val superClass = entity.superType
+        val subClass = entity.subType
+        logger.debug("${superClass.name}(IClass) -> ${entity.name}(IGeneralization) - ${subClass.name}(IClass)")
+        return CreateGeneralizationModel(superClass.name, subClass.name, entity.name)
+    }
+
+    private fun createRealizationModel(entity: IRealization): CreateRealizationModel {
+        val supplierClass = entity.supplier
+        val clientClass = entity.client
+        logger.debug("${supplierClass.name}(IClass) -> ${entity.name}(IRealization) -> ${clientClass.name}(IClass)")
+        return CreateRealizationModel(supplierClass.name, clientClass.name, entity.name)
+    }
+
+    private fun createOperation(entity: IOperation): CreateOperation? {
+        return when (val owner = entity.owner) {
+            is IClass -> {
+                logger.debug("${entity.name}(IOperation) - $owner(IClass)")
+                CreateOperation(owner.name, entity.name, entity.returnTypeExpression)
+            }
+            else -> {
+                logger.debug("${entity.name}(IOperation) - $owner(Unknown)")
+                null
+            }
+        }
+    }
+
+    private fun createAttribute(entity: IAttribute): CreateAttribute? {
+        return when (val owner = entity.owner) {
+            is IClass -> {
+                logger.debug("${entity.name}(IAttribute) - ${owner}(IClass)")
+                CreateAttribute(owner.name, entity.name, entity.typeExpression)
+            }
+            else -> {
+                logger.debug("${entity.name}(IAttribute) - ${owner}(Unknown)")
+                null
+            }
+        }
+    }
+
+    private fun createClassPresentation(entity: INodePresentation): CreateClassPresentation? {
+        return when (val diagram = entity.diagram) {
+            is IClassDiagram -> {
+                when (val model = entity.model) {
+                    is IClass -> {
+                        // TODO: クラスとインタフェースをINodePresentationのプロパティで見分ける
+                        val location = Pair(entity.location.x, entity.location.y)
+                        logger.debug("${entity.label}(INodePresentation)::${model.name}(IClass, ${Pair(entity.width, entity.height)} at ${entity.location})")
+                        CreateClassPresentation(model.name, location, diagram.name)
+                    }
+                    else -> {
+                        logger.debug("${entity.label}(INodePresentation) - $model(Unknown)")
+                        null
+                    }
+                }
+            }
+            else -> {
+                logger.debug("${entity.label}(Unknown)")
+                null
+            }
+        }
+    }
+
+    private fun createLinkPresentation(entity: ILinkPresentation): CreateLinkPresentation? {
+        val source = entity.source.model
+        val target = entity.target.model
+        logger.debug("Model: ${entity.model::class.java}")
+        return when (source) {
+            is IClass -> {
+                when (target) {
+                    is IClass -> {
+                        when (entity.model) {
+                            is IAssociation -> {
+                                logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::IAssociation) - ${target.name}(IClass)")
+                                CreateLinkPresentation(source.name, target.name, "Association", entity.diagram.name)
+                            }
+                            is IGeneralization -> {
+                                logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::IGeneralization) - ${target.name}(IClass)")
+                                CreateLinkPresentation(source.name, target.name, "Generalization", entity.diagram.name)
+                            }
+                            is IRealization -> {
+                                logger.debug("${source.name}(IClass, interface) - ${entity.label}(ILinkPresentation::IRealization) - ${target.name}(IClass)")
+                                CreateLinkPresentation(source.name, target.name, "Realization", entity.diagram.name)
+                            }
+                            else -> {
+                                logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation::Unknown) - ${target.name}(IClass)")
+                                null
+                            }
+                        }
+                    }
+                    else -> {
+                        logger.debug("${source.name}(IClass) - ${entity.label}(ILinkPresentation) - $target(Unknown)")
+                        null
+                    }
+                }
+            }
+            else -> {
+                logger.debug("$source(Unknown) - ${entity.label}(ILinkPresentation) - $target(Unknown)")
                 null
             }
         }
