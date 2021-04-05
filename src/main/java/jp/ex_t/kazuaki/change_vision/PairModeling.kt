746bc3f4693ffe2ed7131216f0dcc111ba6a2233
HyodaKazuaki <kazuaki@ex-t.jp>
2021-04-05 18:27:00 +0900

    Add project sync feature

--- src/main/java/jp/ex_t/kazuaki/change_vision/PairModeling.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/PairModeling.kt
@@ -15,36 +15,60 @@ import jp.ex_t.kazuaki.change_vision.network.*
 
 class PairModeling(topic: String, private val clientId: String, private val brokerAddress: String) {
     private val topicTransaction = "$topic/transaction"
+    private val topicSync = "$topic/sync"
 
     // TODO: もしプロジェクト全体が欲しいとなった場合はトピックを別で生やす
     // TODO: もしチャットが欲しいとなった場合はトピックを別で生やす
     private lateinit var mqttPublisher: MqttPublisher
     private lateinit var projectChangedListener: ProjectChangedListener
     private lateinit var mqttSubscriber: MqttSubscriber
-    private lateinit var transactionReceiver: TransactionReceiver
 
     fun start() {
         val api = AstahAPI.getAstahAPI()
         val projectAccessor = api.projectAccessor
+
+        val projectReceiver = ProjectReceiver()
+        val topicProjectReceiveSubscriber = "$topicSync/$clientId/#"
+        val receiveSubscriberConfig = MqttSubscriberConfig(topicProjectReceiveSubscriber, 2, projectReceiver)
+        val topicSyncSubscriber = "$topicSync/#"
+        val projectSender = ProjectSender(topicSync, clientId, brokerAddress)
+        val projectSenderConfig = MqttSubscriberConfig(topicSyncSubscriber, 2, projectSender)
 
         logger.debug("Launching publisher...")
         val topicTransactionPublisher = "$topicTransaction/$clientId"
         mqttPublisher = MqttPublisher(brokerAddress, topicTransactionPublisher, clientId)
         projectChangedListener = ProjectChangedListener(mqttPublisher)
         projectAccessor.addProjectEventListener(projectChangedListener)
-        logger.debug("Published: $brokerAddress:$topicTransaction ($clientId")
+        logger.debug("Published: $brokerAddress:$topicTransaction ($clientId)")
         logger.info("Launched publisher.")
 
         logger.debug("Launching subscriber...")
         val topicTransactionSubscriber = "$topicTransaction/#"
-        transactionReceiver = TransactionReceiver(projectChangedListener)
-        val mqttSubscriberConfig = MqttSubscriberConfig(topicTransactionSubscriber, 2, transactionReceiver)
-        mqttSubscriber = MqttSubscriber(brokerAddress, listOf(mqttSubscriberConfig), clientId)
+        val transactionReceiver = TransactionReceiver(projectChangedListener)
+        val transactionReceiverConfig = MqttSubscriberConfig(topicTransactionSubscriber, 2, transactionReceiver)
+        val mqttSubscriberConfigList = listOf(
+            receiveSubscriberConfig,
+            projectSenderConfig,
+            transactionReceiverConfig,
+        )
+        mqttSubscriber = MqttSubscriber(brokerAddress, mqttSubscriberConfigList, clientId)
         mqttSubscriber.subscribe()
-        logger.debug("Subscribed: $brokerAddress:$topicTransaction ($clientId")
+        logger.debug("Subscribed: $brokerAddress:$topicTransaction ($clientId)")
         logger.info("Launched subscriber.")
 
         syncProject()
+    }
+
+    private fun syncProject() {
+        logger.debug("Launching publisher...")
+        val topicProjectReceivePublisher = "$topicSync/$clientId"
+        val topicSyncRequester = MqttPublisher(brokerAddress, topicProjectReceivePublisher, clientId)
+        logger.debug("Published: $brokerAddress:$topicSync ($clientId)")
+        logger.info("Launched publisher.")
+
+        logger.debug("Requesting project...")
+        topicSyncRequester.publish("hello".toByteArray())
+        logger.info("Requested project.")
     }
 
     fun end() {
