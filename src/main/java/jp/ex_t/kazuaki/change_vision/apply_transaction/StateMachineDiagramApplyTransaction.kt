ccd2be90be961f560cb31306645869310725c6b2
HyodaKazuaki <kazuaki@ex-t.jp>
2021-04-16 17:15:37 +0900

    Add pseudostate creation feature

--- src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/StateMachineDiagramApplyTransaction.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/StateMachineDiagramApplyTransaction.kt
@@ -10,12 +10,11 @@ package jp.ex_t.kazuaki.change_vision.apply_transaction
 
 import com.change_vision.jude.api.inf.AstahAPI
 import com.change_vision.jude.api.inf.model.INamedElement
+import com.change_vision.jude.api.inf.presentation.INodePresentation
 import jp.ex_t.kazuaki.change_vision.Logging
 import jp.ex_t.kazuaki.change_vision.logger
-import jp.ex_t.kazuaki.change_vision.network.CreateStateMachineDiagram
-import jp.ex_t.kazuaki.change_vision.network.EntityLUT
-import jp.ex_t.kazuaki.change_vision.network.Entry
-import jp.ex_t.kazuaki.change_vision.network.StateMachineDiagramOperation
+import jp.ex_t.kazuaki.change_vision.network.*
+import java.awt.geom.Point2D
 
 class StateMachineDiagramApplyTransaction(private val entityLUT: EntityLUT) :
     IApplyTransaction<StateMachineDiagramOperation> {
@@ -31,6 +30,7 @@ class StateMachineDiagramApplyTransaction(private val entityLUT: EntityLUT) :
                 is CreateStateMachineDiagram -> {
                     validateAndCreateStateMachineDiagram(it)
                 }
+                is CreatePseudostate -> validateAndCreatePseudostate(it)
             }
         }
     }
@@ -41,12 +41,47 @@ class StateMachineDiagramApplyTransaction(private val entityLUT: EntityLUT) :
         }
     }
 
+    private fun validateAndCreatePseudostate(operation: CreatePseudostate) {
+        if (operation.id.isNotEmpty()) {
+            val location = Point2D.Double(operation.location.first, operation.location.second)
+            createPseudostate(operation.id, operation.label, location, operation.size, operation.ownerId)
+        }
+    }
+
     private fun createStateMachineDiagram(name: String, ownerName: String, id: String) {
         logger.debug("Create state machine diagram.")
         val owner = projectAccessor.findElements(INamedElement::class.java, ownerName).first()
         val diagram = stateMachineDiagramEditor.createStatemachineDiagram(owner, name)
         entityLUT.entries.add(Entry(diagram.id, id))
         diagramViewManager.open(diagram)
+    }
+
+    private fun createPseudostate(
+        id: String,
+        label: String,
+        location: Point2D,
+        size: Pair<Double, Double>,
+        parentId: String
+    ) {
+        logger.debug("Create pseudostate.")
+        val (width, height) = size
+        stateMachineDiagramEditor.diagram = diagramViewManager.currentDiagram
+        val parentEntity = if (parentId.isEmpty()) null else entityLUT.entries.find { it.common == parentId } ?: run {
+            logger.debug("$parentId not found on LUT.")
+            return
+        }
+        val parentPresentation =
+            if (parentEntity == null) null else diagramViewManager.currentDiagram.presentations.find { it.id == parentEntity.mine } as INodePresentation?
+                ?: run {
+                    logger.debug("INodePresentation ${parentEntity.mine} not found but $parentId found on LUT.")
+                    return
+                }
+
+        val pseudostate = stateMachineDiagramEditor.createInitialPseudostate(parentPresentation, location)
+        entityLUT.entries.add(Entry(pseudostate.id, id))
+        pseudostate.width = width
+        pseudostate.height = height
+        pseudostate.label = label
     }
 
     companion object : Logging {
