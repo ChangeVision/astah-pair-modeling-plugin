aaf22a3054548bab6b811ce8f2f6e95c9c703787
HyodaKazuaki <kazuaki@ex-t.jp>
2021-06-17 14:59:22 +0900

    Change Receiver to abstract class

--- src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/TransactionReceiver.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/TransactionReceiver.kt
@@ -19,10 +19,14 @@ import jp.ex_t.kazuaki.change_vision.network.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
+import org.eclipse.paho.client.mqttv3.MqttMessage
 import javax.swing.SwingUtilities
 
-class TransactionReceiver(entityLUT: EntityLUT, private val projectChangedListener: ProjectChangedListener) :
-    IReceiver {
+class TransactionReceiver(
+    entityLUT: EntityLUT, private val projectChangedListener: ProjectChangedListener,
+    clientId: String
+) :
+    Receiver(clientId) {
     private val api: AstahAPI = AstahAPI.getAstahAPI()
     private val projectAccessor: ProjectAccessor = api.projectAccessor
     private val commonApplyTransaction = CommonApplyTransaction(entityLUT)
@@ -31,8 +35,9 @@ class TransactionReceiver(entityLUT: EntityLUT, private val projectChangedListen
     private val stateMachineDiagramApplyTransaction = StateMachineDiagramApplyTransaction(entityLUT)
 
     @ExperimentalSerializationApi
-    override fun receive(topic: String, payload: ByteArray) {
-        val receivedMessage = Cbor.decodeFromByteArray<Transaction>(payload)
+    override fun messageArrived(topic: String, message: MqttMessage) {
+        if (isReceivedMyself(topic)) return
+        val receivedMessage = Cbor.decodeFromByteArray<Transaction>(message.payload)
         logger.debug("Received: $receivedMessage ($topic)")
         transact(receivedMessage)
     }
--- src/main/java/jp/ex_t/kazuaki/change_vision/network/MqttSubscriber.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/network/MqttSubscriber.kt
@@ -10,58 +10,33 @@ package jp.ex_t.kazuaki.change_vision.network
 
 import jp.ex_t.kazuaki.change_vision.Logging
 import jp.ex_t.kazuaki.change_vision.logger
-import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
-class MqttSubscriber(
-    brokerAddress: String,
-    brokerPortNumber: Int,
-    private val topic: String,
-    private val clientId: String,
-    private val receiver: IReceiver
-) : MqttCallback {
-    private var broker: String = "tcp://$brokerAddress:$brokerPortNumber"
-    private lateinit var mqttClient: MqttClient
-    private val clientIdSubscriber = "$clientId/sub"
+class MqttSubscriber(brokerAddress: String, brokerPortNumber: Int, clientId: String) {
+    private var mqttClient: MqttClient
 
-    fun subscribe() {
-        val qos = 2
-
+    init {
+        val broker = "tcp://$brokerAddress:$brokerPortNumber"
+        val clientIdSubscriber = "$clientId/sub"
         mqttClient = MqttClient(broker, clientIdSubscriber, MemoryPersistence())
-        mqttClient.setCallback(this)
         val mqttConnectOptions = MqttConnectOptions()
         mqttConnectOptions.isCleanSession = false
         mqttConnectOptions.connectionTimeout = 10
-
         mqttClient.connect(mqttConnectOptions)
         logger.info("Connected to broker $broker")
+    }
 
-        mqttClient.subscribe(topic, qos)
+    fun subscribe(topics: Array<String>, receivers: Array<Receiver>) {
+        val qos = IntArray(topics.size) { 2 }
+        mqttClient.subscribeWithResponse(topics, qos, receivers)
     }
 
     fun close() {
         mqttClient.disconnect()
         mqttClient.close()
         logger.info("Closed connection.")
-    }
-
-    override fun connectionLost(cause: Throwable) {
-        logger.error("Connection lost.", cause)
-        throw cause
-        // TODO: retry?
-    }
-
-    override fun messageArrived(topic: String, message: MqttMessage) {
-        // If the message was send by myself,  ignore this one.
-        val receivedClientId = topic.split("/").last()
-        if (receivedClientId == clientId) {
-            return
-        }
-        receiver.receive(topic, message.payload)
-    }
-
-    override fun deliveryComplete(token: IMqttDeliveryToken) {
-        TODO("Not yet implemented")
     }
 
     companion object : Logging {
