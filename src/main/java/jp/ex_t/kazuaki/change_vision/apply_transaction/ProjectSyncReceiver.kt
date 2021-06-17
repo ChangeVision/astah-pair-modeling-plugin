9ceb9ef923598943f4d7c65bc92b1371265dd8e0
HyodaKazuaki <kazuaki@ex-t.jp>
2021-06-17 17:27:58 +0900

    Add CreateClass when join the room

--- src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/ProjectSyncReceiver.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/apply_transaction/ProjectSyncReceiver.kt
@@ -9,19 +9,23 @@
 package jp.ex_t.kazuaki.change_vision.apply_transaction
 
 import com.change_vision.jude.api.inf.AstahAPI
+import com.change_vision.jude.api.inf.model.IClass
+import com.change_vision.jude.api.inf.model.INamedElement
 import jp.ex_t.kazuaki.change_vision.Logging
 import jp.ex_t.kazuaki.change_vision.logger
-import jp.ex_t.kazuaki.change_vision.network.CreateProject
-import jp.ex_t.kazuaki.change_vision.network.MqttPublisher
-import jp.ex_t.kazuaki.change_vision.network.Receiver
-import jp.ex_t.kazuaki.change_vision.network.Transaction
+import jp.ex_t.kazuaki.change_vision.network.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
-class ProjectSyncReceiver(private val mqttPublisher: MqttPublisher, clientId: String) : Receiver(clientId) {
+class ProjectSyncReceiver(
+    private val entityLUT: EntityLUT,
+    private val mqttPublisher: MqttPublisher,
+    clientId: String
+) :
+    Receiver(clientId) {
     private val api = AstahAPI.getAstahAPI()
     private val projectAccessor = api.projectAccessor
 
@@ -37,11 +41,39 @@ class ProjectSyncReceiver(private val mqttPublisher: MqttPublisher, clientId: St
 
     @ExperimentalSerializationApi
     private fun sync() {
-        // TODO: 1. プロジェクトを新規作成させる
-        // TODO: 2. プロジェクトの起点から一つずつ(対応している)エンティティを送る
         logger.debug("Project Sync")
+        entityLUT.entries.add(Entry(projectAccessor.project.id, projectAccessor.project.id))
         val createProjectTransaction = Transaction(listOf(CreateProject(projectAccessor.project.id)))
         encodeAndPublish(createProjectTransaction)
+
+        // TODO: 2. プロジェクトの起点から一つずつ(対応している)エンティティを送る
+        val rootModel = projectAccessor.project
+        val createOperations = rootModel.ownedElements.mapNotNull {
+            when (it) {
+                is IClass -> createClassModel(it)
+                else -> {
+                    logger.debug("${it}(Unknown)")
+                    null
+                }
+            }
+        }
+        if (createOperations.isNotEmpty()) {
+            val createTransaction = Transaction(createOperations)
+            encodeAndPublish(createTransaction)
+        }
+    }
+
+    private fun createClassModel(entity: IClass): CreateClassModel {
+        entityLUT.entries.add(Entry(entity.id, entity.id))
+//        val parentEntry = entityLUT.entries.find { it.mine == entity.owner.id } ?: run {
+//            logger.debug("${entity.owner.id} not found on LUT.")
+//            return null
+//        }
+        // TODO: ownerのIDをLUTに登録できるようにする(最初はパッケージのIDを登録していないので取れない)
+        val owner = entity.owner as INamedElement
+        val createClassModel = CreateClassModel(entity.name, owner.name, entity.stereotypes.toList(), entity.id)
+        logger.debug("${entity.name}(IClass)")
+        return createClassModel
     }
 
     @ExperimentalSerializationApi
