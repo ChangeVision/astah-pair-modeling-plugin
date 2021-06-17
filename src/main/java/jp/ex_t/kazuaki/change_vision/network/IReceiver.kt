90558abad40d2c3fd388b8fa709adf5cfc95d41b
HyodaKazuaki <kazuaki@ex-t.jp>
2021-06-17 10:46:08 +0900

    Add Receiver interface

---
+++ src/main/java/jp/ex_t/kazuaki/change_vision/network/IReceiver.kt
@@ -0,0 +1,13 @@
+/*
+ * IReceiver.kt - pair-modeling
+ * Copyright © 2021 HyodaKazuaki.
+ *
+ * Released under the MIT License.
+ * see https://opensource.org/licenses/MIT
+ */
+
+package jp.ex_t.kazuaki.change_vision.network
+
+interface IReceiver {
+    fun receive(topic: String, payload: ByteArray)
+}
