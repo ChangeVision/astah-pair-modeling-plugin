4dc83e4b407f620db6bfc1851388baacaf4b6e17
HyodaKazuaki <kazuaki@ex-t.jp>
2021-07-15 15:20:30 +0900

    Add connecting dialog

--- src/main/java/jp/ex_t/kazuaki/change_vision/JoinRoomAction.kt
+++ src/main/java/jp/ex_t/kazuaki/change_vision/JoinRoomAction.kt
@@ -10,9 +10,12 @@ package jp.ex_t.kazuaki.change_vision
 
 import com.change_vision.jude.api.inf.ui.IPluginActionDelegate
 import com.change_vision.jude.api.inf.ui.IWindow
+import kotlinx.coroutines.*
+import kotlinx.coroutines.swing.Swing
 import kotlinx.serialization.ExperimentalSerializationApi
 import java.util.*
 import javax.swing.JOptionPane
+import javax.swing.ProgressMonitor
 import kotlin.io.path.ExperimentalPathApi
 
 class JoinRoomAction : IPluginActionDelegate {
@@ -30,9 +33,41 @@ class JoinRoomAction : IPluginActionDelegate {
                 config.load()
                 val topic = JOptionPane.showInputDialog(window.parent, "Input AIKOTOBA which host tell you.") ?: return
                 val clientId = UUID.randomUUID().toString()
-                pairModeling.join(topic, clientId, config.conf.brokerAddress, config.conf.brokerPortNumber)
-                menuTextChanger.setAfterText(menuId)
-                menuTextChanger.disable(menuId)
+
+                val progressMonitor = ProgressMonitor(window.parent, "Connecting...", "", 0, 100)
+                progressMonitor.millisToDecideToPopup = 0
+                progressMonitor.millisToPopup = 0
+                progressMonitor.setProgress(0)
+                val scope = CoroutineScope(Dispatchers.Default)
+                val job = scope.async {
+                    pairModeling.join(
+                        topic,
+                        clientId,
+                        config.conf.brokerAddress,
+                        config.conf.brokerPortNumber
+                    )
+                }
+                scope.launch(Dispatchers.Swing) {
+                    for (i in 0..100) {
+                        if (job.isCancelled) {
+                            logger.debug("Canceled by system.")
+                            break
+                        }
+                        if (job.isCompleted) {
+                            menuTextChanger.setAfterText(menuId)
+                            menuTextChanger.disable(menuId)
+                            break
+                        }
+                        if (progressMonitor.isCanceled) {
+                            logger.debug("Canceled by progress bar button.")
+                            job.cancel()
+                            break
+                        }
+                        delay(200L)
+                        progressMonitor.setProgress(i)
+                    }
+                }
+                progressMonitor.close()
             } else {
                 pairModeling.end()
                 menuTextChanger.setBeforeText(menuId)
